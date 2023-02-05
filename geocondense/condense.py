6a582846c5b4a9cc90a14626e180b9e237cb05cc
TANG ZHIXIONG <dvora4tzx@gmail.com>
2023-02-05 18:01:27 +0800

    not ready

--- geocondense/condense.py
+++ geocondense/condense.py
@@ -4,17 +4,100 @@ from itertools import chain
 from typing import Any, Dict, List, Optional, Set, Tuple, Union
 
 import numpy as np
+import open3d as o3d
 from loguru import logger
+
+from geocondense.utils import md5sum
+
+
+def resolve_center(
+    center: Optional[Union[str, Tuple[float, float, float]]]
+) -> Optional[Tuple[float, float, float]]:
+    if not center:
+        return None
+    if isinstance(center, str):
+        center = [float(x) for x in center.split(",")]
+    assert isinstance(center, (list, tuple)), f"invalid center: {center}"
+    lon, lat, alt = (*center, 0.0) if len(center) == 2 else tuple(center)
+    return lon, lat, alt
+
+
+def default_handle_semantic(path: str, *, workdir: str, uuid: str) -> Tuple[str, str]:
+    dissect_input_path = path
+    condense_input_path = path
+    return dissect_input_path, condense_input_path
+
+
+def default_handle_pointcloud(
+    path: str,
+    *,
+    workdir: str,
+    uuid: str,
+    center: Optional[Tuple[float, float, float]],
+) -> o3d.geometry.PointCloud:
+    return o3d.io.read_point_cloud(path)
+
+
+def condense_semantic(
+    dissect_input_path: str, condense_input_path: str, *, output_dir: str
+) -> str:
+    raise Exception("not ready")
+
+
+def condense_pointcloud(pcd: o3d.geometry.PointCloud, *, output_dir: str) -> str:
+    raise Exception("not ready")
 
 
 def condense(
     *,
     workdir: str,
-    semantic_files: List[str],
-    pointcloud_files: List[str],
+    semantic_files: List[str] = None,
+    pointcloud_files: List[str] = None,
     center: Optional[Tuple[float, float, float]] = None,
-):
-    pass
+    # handlers
+    handle_semantic=default_handle_semantic,
+    handle_pointcloud=default_handle_pointcloud,
+) -> List[str]:
+    assert not (
+        semantic_files is None and pointcloud_files is None
+    ), f"should specify either --semantic_files or --pointcloud_files"
+    for p in [*(semantic_files or []), *(pointcloud_files or [])]:
+        assert os.path.isfile(p), f"{p} does not exist"
+    center = resolve_center(center)
+    os.makedirs(os.path.abspath(workdir), exist_ok=True)
+    index_list = []
+    for path in semantic_files or []:
+        uuid = md5sum(path)
+        odir = f"{workdir}/{uuid}"
+        sentinel = f"{odir}/condensed"
+        if os.path.isfile(sentinel):
+            logger.info(f"skip condensing {path}, sentinel exists: {sentinel}")
+            continue
+        dissect_input, condense_input = handle_semantic(
+            path,
+            workdir=workdir,
+            uuid=uuid,
+        )
+        index = condense_semantic(dissect_input, condense_input, output_dir=odir)
+        index_list.append(index)
+        Path(sentinel).touch()
+    for path in pointcloud_files or []:
+        uuid = md5sum(path)
+        odir = f"{workdir}/{uuid}"
+        sentinel = f"{odir}/condensed"
+        if os.path.isfile(sentinel):
+            logger.info(f"skip condensing {path}, sentinel exists: {sentinel}")
+            continue
+        pcd = handle_pointcloud(
+            path,
+            workdir=workdir,
+            uuid=uuid,
+            center=center,
+        )
+        index = condense_pointcloud(pcd, output_dir=odir)
+        index_list.append(index)
+        Path(sentinel).touch()
+    return index_list
 
 
 if __name__ == "__main__":
--- geocondense/utils.py
+++ geocondense/utils.py
@@ -1,1 +1,32 @@
-print("hello world")
+from functools import lru_cache
+from sys import platform
+from typing import Any, Dict, List, Optional, Set, Tuple, Union
+
+
+def popen(cmd: Union[str, List[str]]) -> Tuple[int, str, str]:
+    if isinstance(cmd):
+        cmd = cmd.split(" ")
+    p = subprocess.Popen(
+        cmd,
+        stdin=subprocess.DEVNULL,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    out, err = p.communicate()
+    return p.returncode, str(out, "utf-8"), str(err, "utf-8")
+
+
+def md5sum(path: str) -> str:
+    assert os.path.isfile(path), f"{path} does not exist"
+    if platform == "linux" or platform == "linux2":
+        retcode, stdout, stderr = popen(["md5", path])
+        assert retcode == 0
+        md5 = stdout.split()[0]
+    elif platform.startswith("darwin"):
+        retcode, stdout, stderr = popen(["md5sum", path])
+        assert retcode == 0
+        md5 = stdout.strip().split()[-1]
+    else:
+        raise Exception("not ready")
+    assert len(md5) == 32
+    return md5
